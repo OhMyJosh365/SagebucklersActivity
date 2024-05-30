0d8e09f17c5a395386911909a95ee659fd8957fd
Joshua Mason <rrtxpeng@gmail.com>
2024-05-29 23:57:31 -0500

    Initial Commit

---
+++ packages/client/src/main.ts
@@ -0,0 +1,21 @@
+import kaboom from "kaboom"
+import "kaboom/global"
+
+// initialize context
+kaboom()
+
+// load assets
+loadSprite("bean", "sprites/bean.png")
+
+// add a character to screen
+add([
+	// list of components
+	sprite("bean"),
+	pos(80, 40),
+	area(),
+])
+
+// add a kaboom on mouse click
+onClick(() => {
+	addKaboom(mousePos())
+})
---
+++ packages/server/src/shared/hello.ts
@@ -0,0 +1,4 @@
+export function hello() {
+    console.log("hello from the server's shared folder");
+  }
+  
