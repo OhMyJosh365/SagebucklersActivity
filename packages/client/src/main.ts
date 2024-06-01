f0cc97bd267876fe70232157e21884a0eaabe014
Joshua Mason <rrtxpeng@gmail.com>
2024-06-01 14:22:32 -0500

    Added dungeon scene for loading gameplay

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -6,38 +6,42 @@ const k = kaboom({
 	background: "000000"
 })
 
-k.add([
-	k.text("Sagebucklers!"),
-	k.pos(k.center()),
-	k.anchor("center")
-])
+k.scene("dungeon", () => {
+	k.add([
+		k.text("Sagebucklers!"),
+		k.pos(k.center()),
+		k.anchor("center")
+	])
+	
+	k.loadSprite("player", "src/sprites/bean.png")
+	
+	const player = k.add([
+		k.sprite("player"),
+		k.pos(k.center()),
+		k.anchor("center"),
+		k.rotate(0)
+	])
+	
+	k.onKeyDown("w", () => {
+		player.pos.y -= 2;
+	})
+	
+	k.onKeyDown("s", () => {
+		player.pos.y += 2;
+	})
+	
+	k.onKeyDown("a", () => {
+		player.pos.x -= 2;
+	})
+	
+	k.onKeyDown("d", () => {
+		player.pos.x += 2;
+	})
+	
+	// add a kaboom on mouse click
+	k.onClick(() => {
+		addKaboom(mousePos())
+	})
+});
 
-k.loadSprite("player", "src/sprites/bean.png")
-
-const player = k.add([
-	k.sprite("player"),
-	k.pos(k.center()),
-	k.anchor("center"),
-	k.rotate(0)
-])
-
-k.onKeyDown("w", () => {
-	player.pos.y -= 2;
-})
-
-k.onKeyDown("s", () => {
-	player.pos.y += 2;
-})
-
-k.onKeyDown("a", () => {
-	player.pos.x -= 2;
-})
-
-k.onKeyDown("d", () => {
-	player.pos.x += 2;
-})
-
-// add a kaboom on mouse click
-k.onClick(() => {
-	addKaboom(mousePos())
-})
+k.go("dungeon")
